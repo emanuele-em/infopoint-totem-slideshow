7ae534693355405960e02e25a7c907a057ec9595
emanuele-em <micheletti.emanuele@hotmail.com>
2022-08-08 22:26:58 +0200

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,10 +61,6 @@ module.exports = {
     },
     {
       resolve: "gatsby-plugin-netlify-cms",
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
-    "gatsby-plugin-remove-serviceworker"
+    }
   ],
 };
