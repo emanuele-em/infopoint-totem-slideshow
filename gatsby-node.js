f1a004929503c696918ee80fb026fa1cfb1b5efc
emanuele-em <micheletti.emanuele@hotmail.com>
2022-08-08 22:51:19 +0200

    zfdgfhgjn

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,20 +1,57 @@
-// const _ = require('lodash');
-// // const path = require('path');
-// const { createFilePath } = require('gatsby-source-filesystem');
-// const { fmImagesToRelative } = require('gatsby-remark-relative-images');
+const _ = require('lodash');
+const path = require('path');
+const { createFilePath } = require('gatsby-source-filesystem');
 
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions
 
+  return graphql(`
+    {
+      allMarkdownRemark(limit: 1000) {
+        edges {
+          node {
+            id
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then((result) => {
+    if (result.errors) {
+      result.errors.forEach((e) => console.error(e.toString()))
+      return Promise.reject(result.errors)
+    }
 
-// exports.onCreateNode = ({ node, actions, getNode }) => {
-//   const { createNodeField } = actions
-//   fmImagesToRelative(node) // convert image paths for gatsby images
+    const pages = result.data.allMarkdownRemark.edges
+    pages.forEach((edge) => {
+      const id = edge.node.id
+      createPage({
+        path: edge.node.fields.slug,
+        // tags: edge.node.frontmatter.tags,
+        component: path.resolve(
+          `src/pages/index.js`
+        ),
+        // additional data can be passed via context
+        context: {
+          id,
+        },
+      })
+    })
 
-//   if (node.internal.type === `MarkdownRemark`) {
-//     const value = createFilePath({ node, getNode })
-//     createNodeField({
-//       name: `slug`,
-//       node,
-//       value,
-//     })
-//   }
-// }
+  })
+}
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
+}
