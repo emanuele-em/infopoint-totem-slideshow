8b250224503de6b7d8f109f0d040aac2cc886d78
emanuele-em <micheletti.emanuele@hotmail.com>
2022-08-08 17:07:51 +0200

    service worker ready

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,4 @@
-exports.onServiceWorkerUpdateFound = () => {
+exports.onServiceWorkerUpdateReady = () => {
     if (
       window.confirm(
         "This site has been updated with new data. Do you wish to reload the site to get the new data?"
