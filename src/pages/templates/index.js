e7079eaa3d671996c677a1174b207c166d4048ed
emanuele-em <micheletti.emanuele@hotmail.com>
2022-08-09 12:01:33 +0200

    adsfasdfsad

--- src/pages/templates/index.js
+++ src/pages/templates/index.js
@@ -108,7 +108,7 @@ class IndexPage extends React.Component {
   render() {
     console.log(this.props);
     this.slideshow = this.props.pageContext.slideshow;
-    console.log(this.slideshow.filter(onlyDisplaySlide));
+    console.log(this.slideshow);
     //console.log(this.props.pageContext.slideshow.filter(onlyDisplaySlide));
     return (
       <Layout >
